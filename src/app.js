2a8508e748ba3dd3e9239df8ab6ecf1f957b3dfb
Gabi8s8 <gabriela.teixeira1@aluno.ifsp.edu.br>
2024-05-07 08:41:31 -0300

    Configurando o servidor

---
+++ src/app.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import path from 'path';
+import routes from './router';
+import dotenv from 'dotenv';
+
+dotenv.config();
+class App {
+  constructor() {
+    this.server = express(); //ativar o servidor; server é a variavel que tem o express
+    mongoose.connect(process.env.DATABASE, {}); //conexão com banco
+    this.middlewares(); // conectar com middleware
+    this.routes();
+  }
+
+  middlewares() {
+    this.server.use(cors());
+    this.server.use(
+      //server.use = manda para o servidor inteiro
+      '/files',
+      express.static(path.resolve(__dirname, '..', 'uploads')) //caminho pra mandar imagens pra pasta uploads; static = fixo, não vai mudar
+    );
+    this.server.use(express.json())
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+}
+
+export default new App().server
