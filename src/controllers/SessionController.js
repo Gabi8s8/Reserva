549f326889a45c96e1cdea25c8a770439b18c9ae
Gabi8s8 <gabriela.teixeira1@aluno.ifsp.edu.br>
2024-05-13 08:39:51 -0300

    Models

---
+++ src/controllers/SessionController.js
@@ -0,0 +1,16 @@
+/*
+    index (listALL): listagem de sessões
+    store (add): criar uma sessão
+    show (list): quando queremos listar uma UNICA sessão
+    update: quando queremos alterar alguma sessão
+    destroy (delete): quando queremos deletar uma sessão
+*/
+
+import User from '../models/User'
+import * as Yup from 'yup' //carrega todas as funções do yup
+
+class SessionController {
+    async store(req, res) { //async = não sincronizado,tem que esperar
+
+    }
+}
---
+++ src/models/House.js
@@ -0,0 +1,23 @@
+import { Schema, model } from 'mongoose' //chama qual função do pacote mongoose quer carregar
+
+const HouseSchema = new Schema ({ //tipo uma tabela do sql
+    thumbnail: String,
+    description: String,
+    price: Number, 
+    location: String,
+    status: Boolean,
+    user: {
+        type: Schema.Types.ObjectId, //id
+        ref: 'User'  // falar que o produto é o do usuario
+    }
+}, {
+    toJSON: {
+        virtuals: true
+    }
+})
+
+HouseSchema.virtual('thumbnail_url').get(function() {
+    return `http://localhost:4000/files/${this.thumbnail}`
+})
+
+export default model('House', HouseSchema)
