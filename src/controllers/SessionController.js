97d0efed973b446dde7b8eb43e36fa791a8ddf8b
Gabi8s8 <gabriela.teixeira1@aluno.ifsp.edu.br>
2024-05-14 08:43:37 -0300

    Controlador da sessao

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -11,6 +11,21 @@ import * as Yup from 'yup' //carrega todas as funções do yup
 
 class SessionController {
     async store(req, res) { //async = não sincronizado,tem que esperar
-
+        const schema = Yup.object().shape({
+            email: Yup.string().email().required(), //verificar email
+        })
+        const { email } = req.body
+        if(!(await schema.isValid(req.body))) { //executa a validação do email
+            return res.status(400).json({ error: 'Falha na validação'})
+        }
+        let user = await User.findOne({ email }) //findOne = procurar o email
+        if(!user) {
+            user = await User.create({ email }) //se não tiver usuario cadastrado, ent cria
+        }
+        return res.json(user)
     }
 }
+
+export default new SessionController()
+// não pode correr risco de alterar = const
+// não vou usar em lugar nenhum mais dps = let
